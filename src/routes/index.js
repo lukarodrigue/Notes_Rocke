d0582b7f8477828f7b059fcbb332dfac99aa5191
luka <lukarodrigues@gmail.com>
2024-11-06 22:47:10 -0300

    ✨ feat(AddSessionsControllers): Add
    adicionado função para validar email e senha.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
-const { Router } = require('express')
+const { Router } = require("express");
 
-const usersRoutes = require('./users.routes')
-const notesRoutes = require('./notes.routes')
-const tagsRoutes = require('./tags.routes')
+const usersRoutes = require("./users.routes");
+const notesRoutes = require("./notes.routes");
+const tagsRoutes = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
-const routes = Router()
+const routes = Router();
 
-routes.use('/users', usersRoutes)
-routes.use('/notes', notesRoutes)
-routes.use('/tags', tagsRoutes)
+routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionsRouter);
+routes.use("/notes", notesRoutes);
+routes.use("/tags", tagsRoutes);
 
-module.exports = routes
+module.exports = routes;
