aeb94fa5eaecbc3af8b03dbaecc39e95e4df44c9
luka <lukarodrigues@gmail.com>
2024-12-07 01:26:19 -0300

    ✨ feat(Controller and Users att):
    Atualizaçao de rotas e users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,21 +1,18 @@
 const { Router } = require("express");
 const multer = require("multer")
+
 const uploadConfig = require("../configs/upload");
-
-const UsersController = require("../controllers/UsersController");
-
-const UsersAvatarController = require("../controllers/UsersAvatarController");
-
-
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
+const UsersController = require("../controllers/UsersController");
+const UsersAvatarController = require("../controllers/UsersAvatarController");
+
 const usersRoutes = Router();
-const upload = multer(uploadConfig.MULTER);
-
-
 
 const usersController = new UsersController();
 const usersAvatarController = new UsersAvatarController();
+
+const upload = multer(uploadConfig.MULTER);
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
@@ -23,7 +20,5 @@ usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (requ
     console.log(request.file.filename);
     response.json();
 } )
-
-
 
 module.exports = usersRoutes;
